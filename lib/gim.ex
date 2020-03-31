4bd009cd83e427fabdb1f213df329c74c6659d65
Hans Bernhard Gödeke <hgoedeke@frobese.de>
2020-03-31 14:21:45 +0200

    Change mix file, docs and license

---
+++ lib/gim.ex
@@ -0,0 +1,16 @@
+defmodule Gim do
+  @moduledoc """
+  #{Gim.MixProject.project()[:description]}.
+
+  ## Usage
+  Add Gim to your application by adding `{:gim, "~> #{Mix.Project.config()[:version]}"}` to your list of dependencies in `mix.exs`:
+  ```elixir
+  def deps do
+    [
+      # ...
+      {:gim, "~> #{Mix.Project.config()[:version]}"}
+    ]
+  end
+  ```
+  """
+end
--- mix.exs
+++ mix.exs
@@ -1,42 +1,65 @@
 defmodule Gim.MixProject do
   use Mix.Project
 
+  @version "1.0.0"
+
   def project do
     [
       app: :gim,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.5",
-      elixirc_paths: elixirc_paths(Mix.env()),
-      # compilers: [:phoenix, :gettext] ++ Mix.compilers(),
-      compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      docs: docs(),
+      elixirc_paths: elixirc_paths(Mix.env())
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
+  def elixirc_paths(env) when env in [:dev, :test] do
+    ["test/support" | elixirc_paths(nil)]
+  end
+
+  def elixirc_paths(_), do: ["lib"]
+
+  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      # mod: {GimTest.Application, []},
       extra_applications: [:logger, :runtime_tools]
     ]
   end
 
-  # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(:dev), do: ["lib", "test/support"]
-  defp elixirc_paths(_), do: ["lib"]
-
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
+  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.19", only: :dev},
       {:jason, "~> 1.0"},
-      {:observer_cli, "~> 1.5", only: :dev},
-      {:nimble_csv, "~> 0.7", only: [:dev, :test]}
+      {:nimble_csv, "~> 0.7", only: [:dev, :test]},
+      {:observer_cli, "~> 1.5", only: :dev}
+    ]
+  end
+
+  defp description do
+    "Schema-based In-Memory Graph Database."
+  end
+
+  defp docs do
+    [
+      main: "Gim",
+      canonical: "http://hexdocs.pm/gim",
+      source_url: "https://github.com/frobese/gim"
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Christian Zuckschwerdt", "Hans Gödeke"],
+      files: ~w(lib mix.exs README* LICENSE*),
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => "https://github.com/frobese/gim"
+      }
     ]
   end
 end
